fe790814aaa691c0e0f042acd0696a739f35b471
김지원 <kimjiwon@gimjiwon-ui-MacBookAir.local>
2024-08-23 11:50:17 +0900

    first commit

--- app/ssg/[id]/page.tsx
+++ app/ssg/[id]/page.tsx
@@ -1,33 +1,33 @@
-import {RenderingInfo} from '@/ui/rendering-info';
+import { RenderingInfo } from '@/ui/rendering-info';
 import { notFound } from 'next/navigation';
 
 export async function generateStaticParams() {
-    return [{id: 1}, {id: 2}]
+    return [{ id: '1' }, { id: '2' }]; // 숫자를 문자열로 변환
 }
 
-export default async function Page({ params} : {params: {id: string}}) {
+export default async function Page({ params }: { params: { id: string } }) {
     if (Number(params.id) >= 100) {
         notFound();
     }
 
     const res = await fetch(
         `https://jsonplaceholder.typicode.com/posts/${params.id}`,
-    )
-    const data = (await res.json()) as {title: string; body: string}
+    );
+    const data = (await res.json()) as { title: string; body: string };
 
     const isOnDemand = Number(params.id) >= 3;
 
     return (
-        <div className = "grid grid-cols-6 gap-x-6 gap-y-3">
+        <div className="grid grid-cols-6 gap-x-6 gap-y-3">
             <div className="space-y-3 col-span-full lg:col-span-4">
                 <h1 className="text-2xl font-medium text-gray-600 capitalize truncate">
                     {data.title}
                 </h1>
                 <p className="font-medium text-gray-500 line-clamp-3">{data.body}</p>
             </div>
-            <div className="-order-1 col-span-full lg: order-none lg:col-span-2">
+            <div className="-order-1 col-span-full lg:order-none lg:col-span-2">
                 <RenderingInfo type={isOnDemand ? 'ssgod' : 'ssg'} />
             </div>
         </div>
-    )
+    );
 }
